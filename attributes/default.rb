274f67ae2932feb04dc708cdb563f44d0ef5be33
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-04-23 22:46:28 +0100

    Upped the versions of teamcity and youtrack.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-default["cookbook_jetbrains"]["teamcity"]["version"] = "8.0.3"
+default["cookbook_jetbrains"]["teamcity"]["version"] = "8.1.2"
 default["cookbook_jetbrains"]["teamcity"]["address"] = "teamcity.localhost"
 default["cookbook_jetbrains"]["teamcity"]["postgresql-driver-version"] = "9.2-1003"
 
-default["cookbook_jetbrains"]["youtrack"]["version"] = "5.0.3"
+default["cookbook_jetbrains"]["youtrack"]["version"] = "5.2-8723"
 default["cookbook_jetbrains"]["youtrack"]["address"] = "youtrack.localhost"
