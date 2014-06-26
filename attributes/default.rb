0d796b8b8b874e368df038e3a19a6a4a74070aac
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-06-26 16:16:20 +0100

    Upped the versions of teamcity and youtrack and tried to sort the memory requirements out too.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-default["cookbook_jetbrains"]["teamcity"]["version"] = "8.1.2"
+default["cookbook_jetbrains"]["teamcity"]["version"] = "8.1.3"
 default["cookbook_jetbrains"]["teamcity"]["address"] = "teamcity.localhost"
 default["cookbook_jetbrains"]["teamcity"]["postgresql-driver-version"] = "9.2-1003"
 
-default["cookbook_jetbrains"]["youtrack"]["version"] = "5.2-8723"
+default["cookbook_jetbrains"]["youtrack"]["version"] = "5.2.1-8778"
 default["cookbook_jetbrains"]["youtrack"]["address"] = "youtrack.localhost"
